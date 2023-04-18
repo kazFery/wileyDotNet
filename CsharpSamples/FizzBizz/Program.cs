c2a428d5eab617d67fd8d04be9801281787d733f
Farzaneh Kazemi <kazem.farzan@gmail.com>
2023-04-18 10:34:13 -0400

    add fixxbizz

---
+++ CsharpSamples/FizzBizz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBizz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Execute();
+            Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                    Console.WriteLine(i);
+                else if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine($"{i} FizzBizz");
+                else if (i % 3 == 0)
+                    Console.WriteLine($"{i} Fizz");
+                else Console.WriteLine($"{i} Bizz");
+
+
+            }
+        }
+    }
+}
